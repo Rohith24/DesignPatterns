895dac1ead98b8d4e8f6b284b0d6752a825ecc2f
Rohith Reddy <Rohith.c@Technovert.com>
2021-09-16 22:33:44 +0530

    Open close

---
+++ SOLID/OpenClose/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// The Open-Closed Principle states that “software entities such as modules, classes, functions, etc. should be open for extension, but closed for modification“.
+/// </summary>
+namespace OpenClose
+{
+
+    public enum Color
+    {
+        Red, Green, Blue
+    }
+
+    public enum Size
+    {
+        Small, Medium, Large, Yuga
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public Color Color { get; set; }
+        public Size Size { get; set; }
+
+        public Product(string name, Color color, Size size)
+        {
+            if (name == null)
+                throw new ArgumentNullException("name");
+            Name = name;
+            Color = color;
+            Size = size;
+        }
+    }
+    /// <summary>
+    /// If we want create more filters, need to modify this class
+    /// </summary>
+    public class ProductFilter
+    {
+        public IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+        {
+            foreach (var p in products)
+            {
+                if (p.Size == size)
+                    yield return p;
+            }
+        }
+
+        public IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+        {
+            foreach (var p in products)
+            {
+                if (p.Color == color)
+                    yield return p;
+            }
+        }
+
+        public IEnumerable<Product> FilterBySizeAndColor(IEnumerable<Product> products, Size size, Color color)
+        {
+            foreach (var p in products)
+            {
+                if (p.Size == size && p.Color == color)
+                    yield return p;
+            }
+        }
+    }
+
+    #region Open Close Principle
+    public interface ISpecification<T>
+    {
+        bool IsSatisfied(T t);
+    }
+
+    public interface IFilter<T>
+    {
+        IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> specification);
+    }
+
+    public class ColorSpecification : ISpecification<Product>
+    {
+        private Color color;
+        public ColorSpecification(Color color)
+        {
+            this.color = color;
+        }
+        public bool IsSatisfied(Product t)
+        {
+            return t.Color == color;
+        }
+    }
+
+    public class SizeSpecification : ISpecification<Product>
+    {
+        private Size size;
+        public SizeSpecification(Size size)
+        {
+            this.size = size;
+        }
+        public bool IsSatisfied(Product t)
+        {
+            return t.Size == size;
+        }
+    }
+
+    public class AndSpecification<T> : ISpecification<T>
+    {
+        ISpecification<T> first, second;
+
+        public AndSpecification(ISpecification<T> specification1, ISpecification<T> specification2)
+        {
+            first = specification1;
+            second = specification2;
+        }
+
+        public bool IsSatisfied(T t)
+        {
+            return first.IsSatisfied(t) && second.IsSatisfied(t);
+        }
+    }
+
+    public class BetterFilter : IFilter<Product>
+    {
+        public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> specification)
+        {
+            foreach (var i in items)
+            {
+                if (specification.IsSatisfied(i))
+                {
+                    yield return i;
+                }
+            }
+        }
+    }
+
+    #endregion Open Close Principle
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var apple = new Product("Apple", Color.Green, Size.Small);
+            var tree = new Product("Tree", Color.Green, Size.Large);
+            var house = new Product("House", Color.Blue, Size.Large);
+
+            Product[] products = { apple, tree, house };
+
+            var pf = new ProductFilter();
+            Console.WriteLine("Green Products (old):");
+            foreach (var p in pf.FilterByColor(products, Color.Green))
+            {
+                Console.WriteLine(p.Name);
+            }
+            Console.ReadLine();
+
+
+            var bf = new BetterFilter();
+            Console.WriteLine("Green Products (new):");
+            foreach (var p in bf.Filter(products, new ColorSpecification(Color.Green)))
+            {
+                Console.WriteLine(p.Name);
+            }
+            Console.ReadLine();
+
+            Console.WriteLine("Large blue Products (new):");
+            foreach (var p in bf.Filter(products, new AndSpecification<Product>(new ColorSpecification(Color.Blue), new SizeSpecification(Size.Large))))
+            {
+                Console.WriteLine(p.Name);
+            }
+            Console.ReadLine();
+        }
+    }
+}
