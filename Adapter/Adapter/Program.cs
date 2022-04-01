73a2ec1fce8ce58ba1925bd9b5867d1fdd86eaa7
Rohith Reddy <Rohith.c@Technovert.com>
2022-04-02 00:50:52 +0530

    Adapter Pattern

---
+++ Adapter/Adapter/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Adapter
+{
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"({X},{Y})";
+        }
+    }
+    public class Line
+    {
+        public Point Start, End;
+
+        public Line(Point start, Point end)
+        {
+            Start = start;
+            End = end;
+        }
+    }
+
+    public class Vector : Collection<Line>
+    {
+
+    }
+    public class Rectangle : Vector
+    {
+        public Rectangle(int x, int y, int width, int height)
+        {
+            Add(new Line(new Point(x, y), new Point(x + width, y)));
+            Add(new Line(new Point(x + width, y), new Point(x + width, y + height)));
+            Add(new Line(new Point(x, y), new Point(x, y + height)));
+            Add(new Line(new Point(x, y + height), new Point(x + width, y + height)));
+        }
+    }
+
+    public class LineToPointAdapter : Collection<Point>
+    {
+        private static int count;
+        public LineToPointAdapter(Line line)
+        {
+            Console.WriteLine($"{++count}: Generating points for line [{line.Start}, {line.End}]");
+            int left = Math.Min(line.Start.X, line.End.X);
+            int right = Math.Max(line.Start.X, line.End.X);
+            int top = Math.Min(line.Start.Y, line.End.Y);
+            int bottom = Math.Max(line.Start.Y, line.End.Y);
+            int dx = right - left;
+            int dy = line.End.Y - line.Start.Y;
+
+            if (dx == 0)
+            {
+                for (int y = top; y <= bottom; ++y)
+                {
+                    Add(new Point(left, y));
+                }
+            }
+            else if (dy == 0)
+            {
+                for (int x = left; x <= right; ++x)
+                {
+                    Add(new Point(x, top));
+                }
+            }
+        }
+    }
+
+    public class AdpterDemo
+    {
+        private static readonly List<Vector> Vectors = new List<Vector>
+        {
+            new Rectangle(1,1, 10, 10),
+            new Rectangle(3, 3, 6, 6),
+        };
+
+        public static void DrawPoint(Point p)
+        {
+            Console.Write(".");
+        }
+
+        static void Main(string[] args)
+        {
+            foreach (var vo in Vectors)
+            {
+                foreach (var line in vo)
+                {
+                    var adapter = new LineToPointAdapter(line);
+                    foreach (var point in adapter)
+                    {
+                        DrawPoint(point);
+                    }
+                }
+
+            }
+        }
+    }
+}
--- Bulider/Bulider/Creational.Builder.BuilderInheritance.cs
+++ Bulider/Bulider/Creational.Builder.BuilderInheritance.cs
@@ -77,15 +77,15 @@ namespace DesignPatterns
 
         }
 
-        //public static void Main(string[] args)
-        //{
-        //    var me = Person.New
-        //      .Called("Dmitri")
-        //      .WorksAsA("Quant")
-        //      .Born(DateTime.UtcNow)
-        //      .Build();
-        //    Console.WriteLine(me);
+        public static void Main(string[] args)
+        {
+            var me = Person.New
+              .Called("Dmitri")
+              .WorksAsA("Quant")
+              .Born(DateTime.UtcNow)
+              .Build();
+            Console.WriteLine(me);
 
-        //}
+        }
     }
 }
